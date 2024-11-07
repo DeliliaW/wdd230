bb594292da359c0ff601adab4e1c9ef2c215f32b
Delilia Werner <delilia@bellsouth.net>
2024-11-07 15:51:06 -0500

    changed #rangevalues

--- scripts/rangevalue.js
+++ scripts/rangevalue.js
@@ -2,7 +2,6 @@ const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("rating");
 
 // RANGE event listener
-range.addEventListener('change', displayRatingValue);
 range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
