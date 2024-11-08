65e905da0c77fca941c8d7f05077a4b6c1dd7ab4
Delilia Werner <delilia@bellsouth.net>
2024-11-08 01:38:53 -0500

    changed numbervisits.js

--- scripts/numbervisits.js
+++ scripts/numbervisits.js
@@ -1,13 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 
-
-if(numVisits !== 0) {
+if (visitsDisplay) {
+  if(numVisits !== 0) {
     visitsDisplay.textContent = numVisits;
-} else {
+  } else {
     visitsDisplay.textContent = ' First time visitor. Welcome! '
-}
-numVisits++;
+ }
+    numVisits++;
 
-localStorage.setItem("numVisits-ls", numVisits);
+    localStorage.setItem("numVisits-ls", numVisits);
+}
+});    
