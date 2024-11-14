61bb3668de1db99f16b540cef1ba17b7d1a187d0
Delilia Werner <delilia@bellsouth.net>
2024-11-13 23:05:08 -0500

    added to prophets.html and links.js

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,11 @@ const linksURL = "https://deliliaw.github.io/wdd230/lesson09/scripts/data/links.
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
 }
 
 getLinks();
+
+async function displayLinks(_weeks) {
+    
+}
