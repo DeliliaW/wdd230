99b64e7ae9666afff9f5ab5208d6ef9c28c3f886
Delilia Werner <delilia@bellsouth.net>
2024-11-14 23:38:39 -0500

    fixed errors in links.js

--- scripts/links.js
+++ scripts/links.js
@@ -5,28 +5,28 @@ const list = document.querySelector('#card1');
 async function getLinks()   {
  const response = await fetch(linksURL);
  const data = await response.json();
+ displayLinks(data.lessons);
+ console.log(data);
 }
 
- displayLinks(data);
-
  const displayLinks = (weeks) => {
-    weeks.forEach ( (week) => {
-     let lessonElement = document.createElement('ul');
-     let lessonTitle = document.createElement('h3');
-     lessonTitle.textContent = `Lesson $(week.lesson)`;
-     lessonElement.appendChild(lessonTitle);
+    weeks.forEach((week) => {
+        let lessonElement = document.createElement('ul');
+        let lessonTitle = document.createElement('h3');
+        lessonTitle.textContent = `Lesson ${week.lesson}`;
+        lessonElement.appendChild(lessonTitle);
 
-     lesson.links.forEach((link) => {
-        let linkElement = document.createElement('li');
-        let anchor = document.createElement('a');
-        anchor.href = baseURL + link.url;
-        anchor.textContent = link.title;
-        linkElement.appendChild(anchor);
-        lessonElement.appendChild(linkElement);  
+        week.links.forEach((link) => {
+            let linkElement = document.createElement('li');
+            let anchor = document.createElement('a');
+            anchor.href = baseURL + link.url;
+            anchor.textContent = link.title;
+            linkElement.appendChild(anchor);
+            lessonElement.appendChild(linkElement);
+        });
+
+        list.appendChild(lessonElement);
     });
-
-    list.appendChild(lessonElement);
- });
- }
+}
 
 getLinks();
