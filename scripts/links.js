c2d76b9b85281de03ebb2689deafe9c49750a8ff
Delilia Werner <delilia@bellsouth.net>
2024-11-14 22:46:28 -0500

    updated links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,24 @@
 const baseURL = "https://deliliaw.github.io/wdd230/";
 const linksURL = "https://deliliaw.github.io/wdd230/data/links.json"; 
 const list = document.querySelector('#card1');
+
+async function getLinks()   {
+ const response = await fetch(linksURL);
+ const data = await response.json();
+ displayLinks(data);
+
+ const displayLinks = (weeks) => {
+    weeks.forEach ( (week) => {
+     let weekElement = document.createElement('ul');
+     let linkElement = document.createElement('li');
+
+        weekElement.textContent = `week: ${week.weekName}`;
+        linkElement.textContent = `Link: ${week.link}`;
+
+        weekElement.appendChild(linkElement);
+        list.appendChild(weekElement);
+    });
+ }
+}
+
+getLinks();
