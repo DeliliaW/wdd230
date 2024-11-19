240eb2617b434f72f685b5f63605a1ccec632818
Delilia Werner <delilia@bellsouth.net>
2024-11-19 12:34:43 -0500

    corrected weather.js

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,5 +1,5 @@
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-Icon');
+const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=aab874bc1cc2c1c2feb77bf991e1aa7e';
 
@@ -9,6 +9,7 @@ async function apiFetch()   {
     if (response.ok)  {
         const data =await response.json();
         console.log(data);
+        displayResults(data);
     } else {
         throw Error(await response.text() );
     }
@@ -21,9 +22,9 @@ apiFetch ();
 
 function displayResults(data)   {
     currentTemp.innerHTML = `${data.main.temp.toFixed(1)} &deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather(0).icon}@2x.png`;
-    let desc = data.weather[0].value ;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].icon ;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc};`
+    captionDesc.textContent = desc;
 }
