55d96e350a3d38090f403721c682d992671e4920
Delilia Werner <delilia@bellsouth.net>
2024-11-23 12:37:33 -0500

    changed url

--- chamber/scripts/chamber-weather.js
+++ chamber/scripts/chamber-weather.js
@@ -1,16 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const currentTemp = document.querySelector('#current-temp');
-    const weatherIcon = document.querySelector('#weather-icon');
-    const captionDesc = document.querySelector('figcaption');
-    const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=32.59&lon=-83.7&eclude=hourly,daily&appid=d48b633843a424ea53795c45c6bf743';
+
+
+     const currentTemp = document.querySelector('#current-temp');
+     const weatherIcon = document.querySelector('#weather-icon');
+     const captionDesc = document.querySelector('figcaption');
+     const urlW = 'https://api.openweathermap.org/data/2.5/forecast?lat=32.59&lon=-83.7&units=imperial&appid=aab874bc1cc2c1c2feb77bf991e1aa7e';
+    
 
     async function apiFetch() {
         try {
-            const response = await fetch(url);
+            const response = await fetch(urlW);
             if (response.ok) {
                 const data = await response.json();
                 console.log(data);
-                displayResults(data);
+                //displayResults(data);
+                        //first day
+        console.log(`${data.list[0].main.temp}`);
+        console.log(`${data.list[0].weather[0].description}`);
             } else {
             throw Error(await response.text());
         }
@@ -19,8 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 }
 
-    apiFetch();
-
     function displayResults(data) {
         currentTemp.innerHTML = `${data.main.toFixed(1)} &deg;F`; 
         const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`; 
@@ -28,5 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
         weatherIcon.setAttribute('src', iconsrc);
         weatherIcon.setAttribute('alt', desc);
         captionDesc.textContent = desc; 
+
+
+    } 
+
+     apiFetch();
+ 
+
+    function testF() {
+        console.log('Console test');
     }
-});
+    testF();
+
