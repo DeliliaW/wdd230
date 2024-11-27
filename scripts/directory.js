0ab8277ae6aa259841d092774f0a8d093e2a8f2e
Delilia Werner <delilia@bellsouth.net>
2024-11-27 14:45:40 -0500

    fixed format for showList and showGrid functions

--- scripts/directory.js
+++ scripts/directory.js
@@ -58,17 +58,21 @@ const display = document.querySelector("article");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-gridbutton.addEventListener("click", () => {
+
+function showGrid()
+{
 	// example using arrow function
 	display.classList.add("grid");
 	display.classList.remove("list");
-});
+}
 
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
+function showList() 
+{
 	display.classList.add("list");
 	display.classList.remove("grid");
 }
+
+gridbutton.addEventListener("click", showGrid); 
+listbutton.addEventListener("click", showList);
 
 }
