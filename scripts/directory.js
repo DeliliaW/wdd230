73965a4fb1d1c41e584fec05b2b20e032b6b5fc3
Delilia Werner <delilia@bellsouth.net>
2024-11-27 20:01:57 -0500

    added to directory.js, added to index.html

--- scripts/directory.js
+++ scripts/directory.js
@@ -51,8 +51,58 @@ const displayMembers = (members) => {
         displayContainer.appendChild(card);
 
     });
+};
 
-    const gridbutton = document.querySelector("#grid");
+function shuffleArray(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+const spotlightHome = document.querySelector('spotlight-home');
+
+const displaySpotlight = (members) => {
+    shuffleArray(members);
+    let spotlightCount = 0;
+    members.forEach ( (member) =>  {
+        if (spotlightCount == 3 || (member.membershiplevel != "Gold" && member.membershiplevel != "Silver")) {
+            return;
+         
+        }
+        let card = document.createElement('spotlight-home');
+        let image = document.createElement('img');
+        let name = document.createElement('h4');
+        let address = document.createElement('p');
+        let phonenumber = document.createElement('p');
+        let websiteURL = document.createElement('p');
+        let membershiplevel = document.createElement('p');
+
+        image.setAttribute('src', member.logourl);
+        image.setAttribute('alt', `Logo of ${member.name}`);
+        image.setAttribute('loading', 'lazy');
+        name.textContent = `${member.name} `;
+        address.textContent = `${member.address}`;
+        phonenumber.textContent = `${member.phonenumber}`;
+        websiteURL.textContent = `${member.websiteURL}`;
+        membershiplevel.textContent = `${member.membershiplevel} member`;
+        
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phonenumber);
+        card.appendChild(websiteURL);
+        card.appendChild(membershiplevel);
+
+        spotlightHome.appendChild(card);
+
+        spotlightCount++;
+    });
+}
+          
+const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector("article");
 
@@ -74,5 +124,3 @@ function showList()
 
 gridbutton.addEventListener("click", showGrid); 
 listbutton.addEventListener("click", showList);
-
-}
