6999c476588462d6b50a49be98082e2313166986
Delilia Werner <delilia@bellsouth.net>
2024-11-27 21:45:13 -0500

    removed spotlight code, moved async function down

--- scripts/directory.js
+++ scripts/directory.js
@@ -8,19 +8,6 @@ document.querySelectorAll('img').forEach(img => {
     }
   });
 
-async function getMemberData()  {
-    try {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMembers(data.members);
-} catch (error) {
-    console.error(`Error fetching data:`, error);
-}
-}
-
-
-getMemberData();
-
 const displayMembers = (members) => {
     members.forEach ( (member) =>  {
         let card = document.createElement('article');
@@ -49,58 +36,20 @@ const displayMembers = (members) => {
         card.appendChild(membershiplevel);
         displayContainer.appendChild(card);
 
+        async function getMemberData()  {
+            try {
+            const response = await fetch(url);
+            const data = await response.json();
+            displayMembers(data.members);
+        } catch (error) {
+            console.error(`Error fetching data:`, error);
+        }
+        }
+
     });
 };
 
-function shuffleArray(array) {
-    for (var i = array.length - 1; i >= 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
 
-const spotlightHome = document.querySelector('#spotlight-home');
-
-const displaySpotlight = (members) => {
-    shuffleArray(members);
-    let spotlightCount = 0;
-    members.forEach ( (member) =>  {
-        if (spotlightCount == 3 || (member.membershiplevel != "Gold" && member.membershiplevel != "Silver")) {
-            return;
-         
-        }
-        let card = document.createElement('div');
-        card.classList.add('spotlightHome');
-        let image = document.createElement('img');
-        let name = document.createElement('h4');
-        let address = document.createElement('p');
-        let phonenumber = document.createElement('p');
-        let websiteURL = document.createElement('p');
-        let membershiplevel = document.createElement('p');
-
-        image.setAttribute('src', member.logourl);
-        image.setAttribute('alt', `Logo of ${member.name}`);
-        image.setAttribute('loading', 'lazy');
-        name.textContent = `${member.name} `;
-        address.textContent = `${member.address}`;
-        phonenumber.textContent = `${member.phonenumber}`;
-        websiteURL.textContent = `${member.websiteURL}`;
-        membershiplevel.textContent = `${member.membershiplevel} member`;
-        
-        card.appendChild(image);
-        card.appendChild(name);
-        card.appendChild(address);
-        card.appendChild(phonenumber);
-        card.appendChild(websiteURL);
-        card.appendChild(membershiplevel);
-
-        spotlightHome.appendChild(card);
-
-        spotlightCount++;
-    });
-}
           
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
@@ -126,3 +75,4 @@ if (gridbutton) {
 if (listbutton) {
     listbutton.addEventListener("click", showList);
 }
+getMemberData();
